ad3c962a0bee43beeeb1e8da46f21238b795f935
Dominik Strohmeier <onkeltom@users.noreply.github.com>
2017-11-13 15:45:28 +0100

    Add files via upload

--- chrome_loadtest.py
+++ chrome_loadtest.py
@@ -1,54 +1,84 @@
 import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import csv
 import json
+import os
+import random
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# define unique results file for this run
+ts = int(time.time())
+filename = "perftimings_chrome_" + str(ts) + ".csv"
+
+# initialize Chrome options
 chrome_options = Options()
 
-# Private browsing
+# add option to start Chrome in Incognito mode
+# comment this line if you want to test normal Chrome mode
 chrome_options.add_argument("--incognito")
 
-ts = int(time.time())
-filename = "perftimings" + "_chrome_cold_DSL_" + str(ts) + ".csv"
-
+# open list of urls for testing    
 with open('news.txt', 'r') as url_file:
     test_urls = url_file.readlines()
 
-for x in range(0,50):
+# do 10 runs
+for x in range(0,10):
+    
+    # start chromedriver using the previously defined options
     driver = webdriver.Chrome(chrome_options=chrome_options)  # Optional argument, if not specified will search path.
     
-    print x
     
-    driver.set_page_load_timeout(30)
+    # set page load time out to 60 seconds
+    driver.set_page_load_timeout(60)
+    
+    # randomly shuffle list of urls to avoid order bias in testing
+    random.shuffle(test_urls)
 
+    
     for i, url in enumerate(test_urls):
                 
         try:
+            # request url from list
             driver.get(url)
-            navStart = driver.execute_script("return window.performance.timing")
-            navStart['url'] = url
-            navStart['run'] = str(x)
-        
-            if x == 0 and i == 0:
-                with open(filename, 'wb') as results_file:
-                    csvwriter = csv.writer(results_file)
-                    csvwriter.writerow(navStart.keys())
-                    csvwriter.writerow(navStart.values())
-            else:
+            
+            # pull window.performance.timing after loading the page and add information about url and number of run
+            perf_timings = driver.execute_script("return window.performance.timing")
+            perf_timings['url'] = str(url)
+            perf_timings['run'] = str(x)
+            
+            # write perf_timings to results csv file
+            if os.path.exists(filename):
+                #append_write = 'a' # append if already exists
                 with open(filename, 'a') as results_file:
                     csvwriter = csv.writer(results_file)
-                    csvwriter.writerow(navStart.values())
+                    csvwriter.writerow(perf_timings.values())
+            else:
+                #append_write = 'w' # make a new file if not
+                with open(filename, 'wb') as results_file:
+                    csvwriter = csv.writer(results_file)
+                    csvwriter.writerow(perf_timings.keys())
+                    csvwriter.writerow(perf_timings.values())
 
 
-        except:
-            navStart = driver.execute_script("return window.performance.timing")
-            navStart['url'] = url
-            navStart['run'] = str(x)
-        
-            with open(filename, 'a') as results_file:
-                csvwriter = csv.writer(results_file)
-                csvwriter.writerow(navStart.values())
+        except: # what to do in case that an exception is thrown (which happens usually upon page load timeout)
+            
+            # also pull data and store it to the results file
+            perf_timings = driver.execute_script("return window.performance.timing")
+            perf_timings['url'] = str(url)
+            perf_timings['run'] = str(x)
+            
+            if os.path.exists(filename):
+                #append_write = 'a' # append if already exists
+                with open(filename, 'a') as results_file:
+                    csvwriter = csv.writer(results_file)
+                    csvwriter.writerow(perf_timings.values())
+            else:
+                #append_write = 'w' # make a new file if not
+                with open(filename, 'wb') as results_file:
+                    csvwriter = csv.writer(results_file)
+                    csvwriter.writerow(perf_timings.keys())
+                    csvwriter.writerow(perf_timings.values())
 
             continue
 			  
--- ff_loadtest.py
+++ ff_loadtest.py
@@ -1,63 +1,87 @@
 import time
+import csv
+import random
+import json
+import os
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 
-import csv
-import json
-
+# define unique results file for this run
 ts = int(time.time())
+filename = "perftimings_firefox_" + str(ts) + ".csv"
 
-filename = "perftimings" + "_firefox_cold_DSL" + str(ts) + ".csv"
-
+# initialize Firefox options
 ff_options = Options()
 
-ff_binary = '/Applications/Firefox Beta.app/Contents/MacOS/firefox'
-#ff_binary = '/Applications/FirefoxNightly.app/Contents/MacOS/firefox'
-
-# Private browsing
+# Enables private browsing for testing
+# comment this line if you want to test normal Firefox mode
 ff_options.add_argument("-private")
-#ff_options.setBinary('/Applications/FirefoxNightly.app/Contents/MacOS/firefox')
 
+# Select path to Firefox binary
+# this is the default path on macOS
+ff_binary = '/Applications/Firefox.app/Contents/MacOS/firefox'
+
+# open list of urls for testing
 with open('news.txt', 'r') as url_file:
     test_urls = url_file.readlines()
 
-for x in range(0,50):
-    print(x+1)
+# do 10 runs
+for x in range(0,10):
     
-    driver = webdriver.Firefox(firefox_options=ff_options, firefox_binary = ff_binary )  # Optional argument, if not specified will search path.
+    # start geckodriver using the previously defined options and binary
+    driver = webdriver.Firefox(firefox_options=ff_options, firefox_binary = ff_binary )
     
-#    print x
+    # set page load time out to 60 seconds
+    driver.set_page_load_timeout(60)
     
-    driver.set_page_load_timeout(30)
+    # randomly shuffle list of urls to avoid order bias in testing
+    random.shuffle(test_urls)
 
     for i, url in enumerate(test_urls):
                     
         try:
+            # request url from list
             driver.get(url)
-            navStart = driver.execute_script("return window.performance.timing")
-            navStart['url'] = url
-            navStart['run'] = str(x)
             
-            if x == 0 and i == 0:
-                with open(filename, 'wb') as results_file:
-                    csvwriter = csv.writer(results_file)
-                    csvwriter.writerow(navStart.keys())
-                    csvwriter.writerow(navStart.values())
-            else:
+            # pull window.performance.timing after loading the page and add information about url and number of run
+            perf_timings = driver.execute_script("return window.performance.timing")
+            perf_timings['url'] = str(url)
+            perf_timings['run'] = str(x)
+            
+            # write perf_timings to results csv file
+            if os.path.exists(filename):
+                #append_write = 'a' # append if already exists
                 with open(filename, 'a') as results_file:
                     csvwriter = csv.writer(results_file)
-                    csvwriter.writerow(navStart.values())
+                    csvwriter.writerow(perf_timings.values())
+            else:
+                #append_write = 'w' # make a new file if not
+                with open(filename, 'w') as results_file:
+                    csvwriter = csv.writer(results_file)
+                    csvwriter.writerow(perf_timings.keys())
+                    csvwriter.writerow(perf_timings.values())
 
 
-        except:
-            navStart = driver.execute_script("return window.performance.timing")
-            navStart['url'] = url
-            navStart['run'] = str(x)
+        except: # what to do in case that an exception is thrown (which happens usually upon page load timeout)
+
+            # also pull data and store it to the results file
+            perf_timings = driver.execute_script("return window.performance.timing")
+            perf_timings['url'] = str(url)
+            perf_timings['run'] = str(x)
             
-            with open(filename, 'a') as results_file:
-                csvwriter = csv.writer(results_file)
-                csvwriter.writerow(navStart.values())
+            if os.path.exists(filename):
+                #append_write = 'a' # append if already exists
+                with open(filename, 'a') as results_file:
+                    csvwriter = csv.writer(results_file)
+                    csvwriter.writerow(perf_timings.values())
+            else:
+                #append_write = 'w' # make a new file if not
+                with open(filename, 'w') as results_file:
+                    csvwriter = csv.writer(results_file)
+                    csvwriter.writerow(perf_timings.keys())
+                    csvwriter.writerow(perf_timings.values())
 
             continue
 			  
